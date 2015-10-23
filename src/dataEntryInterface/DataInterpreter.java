ae6b0f966970fb8161c885d25be435ec55098138
DanielOpazo <daniel.opazo.baer@gmail.com>
2015-10-23 01:58:04 -0400

    Refactored DataInterpreter

--- src/dataEntryInterface/DataInterpreter.java
+++ src/dataEntryInterface/DataInterpreter.java
@@ -22,16 +22,42 @@ public class DataInterpreter {
 		setFileData(new DataFromFile(fileName));
 	}
 	
+	/**
+	 * 
+	 * @return the value of the title field. Null if no value, or value is ""
+	 */
 	public String readTitle() {
-		String title = getFileData().getValuesHash().get(titleKey);
-		if (title != null && !title.equalsIgnoreCase("")) {
-			if (title.length() > maxTitleLength) {
-				getLog().warning("Invalid title: title too long. Returning null title");
-				return null;
-			}
-			return title;
+		String title = getValueOrNull(titleKey);
+		if (title == null || title.length() > maxTitleLength) {
+			getLog().warning("Invalid title");
+			return null;
+		}
+		return title;
+	}
+	
+	/**
+	 * 
+	 * @return the value of the title field. 
+	 */
+	public String readArtist() {
+		String artist = getValueOrNull(artistKey);
+		if (artist != null) {
+			return artist;
+		}
+		return "";
+	}
+	
+	/**
+	 * 
+	 * @param key The key of the value to be pulled from the hash
+	 * @return the value corresponding to the key. Null if empty or blank
+	 */
+	private String getValueOrNull(String key) {
+		String value = getFileData().getValuesHash().get(key);
+		if (value != null && !value.equalsIgnoreCase("")) {
+			return value;
 		}else {
-			getLog().warning("Invalid title: title is null or empty. Returning null title");
+			getLog().warning("Invalid " + key + ": " + "value is null or empty. Returning null value");
 			return null;
 		}
 	}
